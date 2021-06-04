8264dc601d62333cb036955cc9b22bc2d9d5e99e
Robert Ross <robert@firehydrant.io>
2021-06-04 09:59:33 -0500

    Break grape

--- lib/grape/dsl/headers.rb
+++ lib/grape/dsl/headers.rb
@@ -6,11 +6,6 @@ module Grape
       # Set an individual header or retrieve
       # all headers that have been set.
       def header(key = nil, val = nil)
-        if key
-          val ? header[key.to_s] = val : header.delete(key.to_s)
-        else
-          @header ||= {}
-        end
       end
       alias headers header
     end
